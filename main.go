f99721d6aac7b50f8b004c888e36934cb4a93739
Stephen Gallagher <stephen.gallagher@gmail.com>
2026-03-15 23:38:51 +0000

    Update route paths

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-// define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
 func main() {
@@ -16,7 +30,9 @@ func main() {
 	// then register the home function as the handler for the
 	// "/" URL pattern
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Print a log message to say that the server is starting
 	log.Print("starting server on :4000")
