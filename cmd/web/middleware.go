52981f6ba84172d162d48539ffff04a235dcd5b5
Stephen Gallagher <stephen.gallagher@gmail.com>
2026-03-21 19:33:00 +0000

    Add middleware layer

---
+++ cmd/web/middleware.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func commonHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+		w.Header().Set("Server", "Go")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.URL.RequestURI()
+		)
+
+		app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// create a deferred handler that will always be run in the event of a panic
+		defer func() {
+			// use the built-in recover()
+			// if a panic did happen, recover() will return the panic value
+			// if a panic didn't happen, it will return nil
+			pv := recover()
+
+			// if a panic did happen..
+			if pv != nil {
+				// set a "Connection: close" header on the response
+				w.Header().Set("Connection", "close")
+				// call the app.serverError helper method to return a 500
+				// internal server response
+				app.serverError(w, r, fmt.Errorf("%v", pv))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
